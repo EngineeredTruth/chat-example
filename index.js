64425cfdb20919f67631cb3229031bddc1183c7c
Jeremy R <jeremy@devmounta.in>
2015-08-11 09:31:21 -0600

    steps

--- index.js
+++ index.js
@@ -2,15 +2,9 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+//One
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
+//Four
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
